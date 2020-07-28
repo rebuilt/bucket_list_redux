f0fe0b2edea632674757c0fb3a890c43b69263cc
rebuilt <memoryman51@hotmail.com>
2020-07-28 11:38:43 +0200

    added root and ideas routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,17 @@
 Rails.application.routes.draw do
+  root to: 'home#index'
+
+  get 'home/index'
+
   get 'styles/atoms'
 
   get 'styles/molecules'
 
   get 'styles/organisms'
+
+  get 'ideas/index'
+
+  get 'ideas', to: 'ideas#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
