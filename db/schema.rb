c18e0f83cc615f682eac7ea9e03513320f4ef959
rebuilt <memoryman51@hotmail.com>
2020-07-30 13:56:46 +0200

    added done_count and photo_url to idea

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200729123844) do
+ActiveRecord::Schema.define(version: 20200730115541) do
 
   create_table "ideas", force: :cascade do |t|
     t.string "title"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "done_count"
+    t.string "photo_url"
   end
 
 end
