63a57ab4f0cf70a2a80a6fab5f5cf45ae5f180ae
rebuilt <memoryman51@hotmail.com>
2020-07-31 23:14:12 +0200

    added style guide navigation test

--- test/system/simple_navigations_test.rb
+++ test/system/simple_navigations_test.rb
@@ -8,4 +8,25 @@ class SimpleNavigationsTest < ApplicationSystemTestCase
     assert has_content?('Spain')
     assert current_url.include?('q=Spain')
   end
+
+  test 'style guide navigation' do
+    visit(root_path)
+    # sleep(1.seconds)
+    click_on "Style Guide", match: :first
+    assert page.has_content? 'Atoms'
+    assert page.has_content? 'Molecules'
+    assert page.has_content? 'Organisms'
+    # sleep(1.seconds)
+    click_on "Molecules"
+    assert page.has_content? 'Card'
+    # sleep(1.seconds)
+    click_on "Organisms", match: :first
+    assert page.has_content? 'Create Goal'
+    # sleep(1.seconds)
+    click_on "My Bucket List", match: :first
+    assert page.has_content? 'Some ideas'
+    # sleep(1.seconds)
+
+
+  end
 end
