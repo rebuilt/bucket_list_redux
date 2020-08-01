1886a257fc608a41bea18b819474bca3b1b95fa9
rebuilt <memoryman51@hotmail.com>
2020-08-01 18:43:14 +0200

    also search in description

--- app/models/idea.rb
+++ app/models/idea.rb
@@ -1,6 +1,6 @@
 class Idea < ApplicationRecord
   def self.search(search_term)
-    where('title LIKE ?', "%#{search_term}%")
+    where('title LIKE ?', "%#{search_term}%").or(where('description LIKE ?', "%#{search_term}%"))
   end
 
   def self.most_recent
