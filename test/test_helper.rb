54606c379f42e940602b212c5bedf4743c42e146
rebuilt <memoryman51@hotmail.com>
2020-08-01 18:38:37 +0200

    convenience method to create an idea

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -4,7 +4,12 @@ require 'rails/test_help'
 
 class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
-  fixtures :all
+  # fixtures :all
 
   # Add more helper methods to be used by all tests here...
+  def create_idea(title)
+    idea = Idea.new(title: title)
+    idea.save!
+    idea
+  end
 end
