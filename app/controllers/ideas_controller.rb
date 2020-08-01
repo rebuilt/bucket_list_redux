d46bd036c3e6fad2d4801314890c912149cb9762
rebuilt <memoryman51@hotmail.com>
2020-08-01 19:48:41 +0200

    set strong params

--- app/controllers/ideas_controller.rb
+++ app/controllers/ideas_controller.rb
@@ -4,33 +4,33 @@ class IdeasController < ApplicationController
     @ideas = Idea.search(@search_term)
   end
 
-  def new ; end
-
-  def edit
-    @idea = Idea.find(params[:id])
-    puts @idea
-
-  end
-
   def show
     @idea = Idea.find(params[:id])
   end
 
-  def update
-    idea = Idea.find(params[:id])
-    idea.title = params[:title]
-    idea.done_count = params[:done_count]
-    idea.photo_url = params[:photo_url]
-    idea.save!
-    redirect_to account_ideas_path
-  end
+  def new ; end
 
   def create
-    idea = Idea.new
-    idea.title = params[:title]
-    idea.done_count = params[:done_count]
-    idea.photo_url = params[:photo_url]
+    idea = Idea.new(params)
     idea.save!
     redirect_to(ideas_path)
   end
+
+  def edit
+    @idea = Idea.find(params[:id])
+    puts @idea
+  end
+
+  def update
+    idea = Idea.find(params[:id])
+    idea.update(idea_params)
+    redirect_to account_ideas_path
+  end
+
+  private 
+
+  def idea_params
+    params.permit(:title, :description, :photo_url )
+  end
+
 end
