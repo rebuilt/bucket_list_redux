62e1a345b3f768da19a3379eff033d8e1798cc6e
rebuilt <memoryman51@hotmail.com>
2020-08-01 23:56:37 +0200

    added validation for title

--- app/models/idea.rb
+++ app/models/idea.rb
@@ -1,4 +1,7 @@
 class Idea < ApplicationRecord
+  validates :title, presence: true
+  validates :title, length: {maximum: 75}
+
   def self.search(search_term)
     where('title LIKE ?', "%#{search_term}%").or(where('description LIKE ?', "%#{search_term}%"))
   end
