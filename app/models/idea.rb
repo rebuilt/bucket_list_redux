1220108f7e760f5e503625b309baa2589802327e
rebuilt <memoryman51@hotmail.com>
2020-08-01 13:52:04 +0200

    added search and most_recent class methods

--- app/models/idea.rb
+++ app/models/idea.rb
@@ -1,2 +1,9 @@
 class Idea < ApplicationRecord
+  def self.search(search_term)
+    where('title LIKE ?', "%#{search_term}%")
+  end
+
+  def self.most_recent
+    Idea.all.limit(3).order(created_at: :desc)
+  end
 end
