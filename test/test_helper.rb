e230f574841dad24e57c7e38790c860ddb05ff2a
rebuilt <memoryman51@hotmail.com>
2020-08-02 20:08:15 +0200

    formatting

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -7,20 +7,20 @@ class ActiveSupport::TestCase
   # fixtures :all
 
   # Add more helper methods to be used by all tests here...
-  def create_idea(title = "default title", description = "default description", photo_url = "http://fpoimg.com/255x170?text=Preview", done_count = 666)
-    user = User.new(email: 'me@mail.com')
+  def create_idea(title = "default title", description = "default description", photo_url = "http://fpoimg.com/255x170?text=Preview", done_count = 666, user = create_user)
     idea = Idea.new(title: title, description: description, photo_url: photo_url, done_count: done_count, user: user)
     idea.save!
     idea
   end
 
   def sign_in
- visit(new_user_path)   
- fill_in('user_email', with: 'me@mail.com')
- click_on('Log in')
+    visit(new_user_path)   
+    fill_in('user_email', with: 'me@mail.com')
+    click_on('Log in')
   end
 
   def create_user(email = 'me@mail.com')
     User.new(email: email)
   end
+
 end
