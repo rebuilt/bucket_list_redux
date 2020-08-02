87598cfb3aac90d00c68ef1800a86f5b813089e3
rebuilt <memoryman51@hotmail.com>
2020-08-02 19:34:19 +0200

    added test to verify comments are ordered correctly

--- test/models/comment_test.rb
+++ test/models/comment_test.rb
@@ -3,16 +3,18 @@ require 'test_helper'
 class CommentTest < ActiveSupport::TestCase
   test 'changing the associated Idea for a Comment' do 
     idea = create_idea("Great idea")
-    comment = Comment.new(body: "I'd like to do this!", idea: idea)
+    user = User.new(email: 'me@mail.com')
+    comment = Comment.new(body: "I'd like to do this!", idea: idea, user: user)
     idea2 = create_idea("Second idea")
     comment.idea = idea2
-    comment.save
+    comment.save!
     assert_equal comment.idea, idea2
   end
 
   test 'cascading save' do 
     idea = create_idea("Great idea")
-    comment = Comment.new(body: "Great idea!")
+    user = User.new(email: 'me@mail.com')
+    comment = Comment.new(body: "Great idea!", user: user)
     idea.comments << comment
     idea.save!
     assert_equal Comment.first, comment
@@ -20,11 +22,12 @@ class CommentTest < ActiveSupport::TestCase
 
   test 'Comments are ordered correctly' do 
     idea = create_idea("Great idea")
-    comment = Comment.new(body: "This would be great fun")
-    comment2 = Comment.new(body: "I agree!  I'd like to do this as well")
+    user = User.new(email: 'me@mail.com')
+    comment = Comment.new(body: "This would be great fun", user: user)
+    comment2 = Comment.new(body: "I agree!  I'd like to do this as well", user: user)
     idea.comments << comment
     idea.comments << comment2
-    idea.save!
+    idea.save
 
     assert_equal idea.comments[0], comment
     assert_equal idea.comments.length, 2 
