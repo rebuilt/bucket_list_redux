dcf6bc452cf3db2dc2d960dc54aef1aa617d24d0
rebuilt <memoryman51@hotmail.com>
2020-08-02 11:55:39 +0200

    added has_many comments association

--- app/models/idea.rb
+++ app/models/idea.rb
@@ -2,6 +2,8 @@ class Idea < ApplicationRecord
   validates :title, presence: true
   validates :title, length: {maximum: 75}
 
+  has_many :comments
+
   def self.search(search_term)
     where('title LIKE ?', "%#{search_term}%").or(where('description LIKE ?', "%#{search_term}%"))
   end
