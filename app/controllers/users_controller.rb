2f55525a94f182bb2839fb99c424c677650347f2
rebuilt <memoryman51@hotmail.com>
2020-08-02 19:32:53 +0200

    save user id to session

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,8 +2,10 @@ class UsersController < ApplicationController
   def new
     @user = User.new
   end
+
   def create
     user = User.find_or_create_by(user_params)
+    session[:user_id] = user.id
     redirect_to ideas_path
   end
 
