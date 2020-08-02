52b8a020aaaf2574172596321be227b8016f494c
rebuilt <memoryman51@hotmail.com>
2020-08-02 12:30:16 +0200

    implemented comments_controller create action

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,13 @@
+class CommentsController < ApplicationController
+  def create
+    @idea = Idea.find(params[:idea_id])
+    @comment = Comment.new(comment_params)
+    @comment.idea = @idea
+    @comment.save
+    redirect_to(idea_path(@idea))
+  end
+
+  def comment_params 
+    params.require(:comment).permit(:body)
+  end
+end
