c300a2c82b48d55e6c2698dd3addfc4b01b077af
rebuilt <memoryman51@hotmail.com>
2020-08-02 20:28:05 +0200

    added user association

--- app/models/idea.rb
+++ app/models/idea.rb
@@ -3,6 +3,9 @@ class Idea < ApplicationRecord
   validates :title, length: {maximum: 75}
 
   has_many :comments
+  belongs_to :user
+
+  has_and_belongs_to_many :users
 
   def self.search(search_term)
     where('title LIKE ?', "%#{search_term}%").or(where('description LIKE ?', "%#{search_term}%"))
