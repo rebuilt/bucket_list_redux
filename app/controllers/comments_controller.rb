a8b8c8a4108653cf6927984c72775b60c8e581ad
rebuilt <memoryman51@hotmail.com>
2020-08-02 19:30:31 +0200

    added user variable to populate idea column

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,7 +1,9 @@
 class CommentsController < ApplicationController
   def create
+    @user = User.find(session[:user_id])
     @idea = Idea.find(params[:idea_id])
     @comment = Comment.new(comment_params)
+    @comment.user = @user
     @comment.idea = @idea
     @comment.save
     redirect_to(idea_path(@idea))
