f51490757b76dfd3232b4863074a041d1945177f
rebuilt <memoryman51@hotmail.com>
2020-08-06 19:05:44 +0200

    add validations and downcase method

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,17 @@
 class User < ApplicationRecord
   has_many :comments
   has_many :ideas
+  before_validation :downcase_email
   validates :email, uniqueness: true
+  validates :email, :password, presence: true
 
   has_secure_password
 
   has_and_belongs_to_many :goals, class_name: 'Idea'
+
+  private
+
+  def downcase_email
+    self.email = email.downcase
+  end
 end
