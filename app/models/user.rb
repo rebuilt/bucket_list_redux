14babdc0c49c034d9f49d33f822809305f9c81a9
rebuilt <memoryman51@hotmail.com>
2020-08-06 11:25:22 +0200

    added bcrypt to gemfile, added password field to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ApplicationRecord
   has_many :comments
   has_many :ideas
+  validates :email, uniqueness: true
+
+  has_secure_password
 
   has_and_belongs_to_many :goals, class_name: 'Idea'
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200802181418) do
+ActiveRecord::Schema.define(version: 20200806090037) do
 
   create_table "comments", force: :cascade do |t|
     t.string "body"
@@ -42,6 +42,7 @@ ActiveRecord::Schema.define(version: 20200802181418) do
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password_digest"
   end
 
 end
