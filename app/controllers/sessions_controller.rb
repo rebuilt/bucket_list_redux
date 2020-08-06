dd3be1fc7f720c0b0f9764c3e084fa0f00474d95
rebuilt <memoryman51@hotmail.com>
2020-08-06 12:21:30 +0200

    created sessions controller

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,15 @@
+class SessionsController < ApplicationController
+  def new; end
+
+  def create
+    user = User.find_by(email: params[:email])
+
+    if user.present? && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect_to account_ideas_path
+    else
+      flash[:alert] = 'Email or password were invalid.  Please try again'
+      render 'new'
+    end
+  end
+end
