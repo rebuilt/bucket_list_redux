2e5331c210cca07279fb898a94d2bd5def3de8b7
rebuilt <memoryman51@hotmail.com>
2020-08-07 23:58:13 +0200

    formatting

--- test/system/simple_navigations_test.rb
+++ test/system/simple_navigations_test.rb
@@ -1,4 +1,4 @@
-require "application_system_test_case"
+require 'application_system_test_case'
 
 class SimpleNavigationsTest < ApplicationSystemTestCase
   test 'search term is displayed' do
@@ -11,15 +11,15 @@ class SimpleNavigationsTest < ApplicationSystemTestCase
 
   test 'style guide navigation' do
     visit(root_path)
-    click_on "Style Guide", match: :first
+    click_on 'Style Guide', match: :first
     assert page.has_content? 'Atoms'
     assert page.has_content? 'Molecules'
     assert page.has_content? 'Organisms'
-    click_on "Molecules"
+    click_on 'Molecules'
     assert page.has_content? 'Card'
-    click_on "Organisms", match: :first
+    click_on 'Organisms', match: :first
     assert page.has_content? 'Create Goal'
-    click_on "My Bucket List", match: :first
+    click_on 'My Bucket List', match: :first
     assert page.has_content? 'Some ideas'
   end
 end
