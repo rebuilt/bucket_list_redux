30c2f8f1c17a679690af2d27fe38843daf349a42
rebuilt <memoryman51@hotmail.com>
2020-08-07 22:11:17 +0200

    add role to user

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200806174812) do
+ActiveRecord::Schema.define(version: 20200807140301) do
 
   create_table "comments", force: :cascade do |t|
     t.string "body"
@@ -45,6 +45,7 @@ ActiveRecord::Schema.define(version: 20200806174812) do
     t.string "password_digest"
     t.string "name"
     t.string "avatar_url"
+    t.string "role"
   end
 
 end
