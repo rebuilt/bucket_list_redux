ef83bc26f00a9b56e00fbd8f9318c99f0ed64eca
rebuilt <memoryman51@hotmail.com>
2020-08-07 12:55:10 +0200

    added idea, edit, update, goals actions

--- app/controllers/account_controller.rb
+++ app/controllers/account_controller.rb
@@ -1,5 +1,24 @@
 class AccountController < ApplicationController
+  before_action :ensure_authenticated
   def ideas
-    @ideas = Idea.all
+    user = current_user
+    @ideas = user.ideas
+  end
+
+  def edit; end
+
+  def update
+    current_user.update(user_params)
+    redirect_to account_path
+  end
+
+  def goals
+    @goals = current_user.goals
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :avatar_url)
   end
 end
