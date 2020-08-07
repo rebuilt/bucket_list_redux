5f19f0bc834aad2693fa09dd7fa04538fc371661
rebuilt <memoryman51@hotmail.com>
2020-08-07 12:55:57 +0200

    added create action

--- app/controllers/goals_controller.rb
+++ app/controllers/goals_controller.rb
@@ -1,9 +1,7 @@
 class GoalsController < ApplicationController
-
   def create
-    user = User.find(params[:user_id])
     idea = Idea.find(params[:idea_id])
-    user.goals << idea
+    current_user.goals << idea
     redirect_to(idea_path(idea))
   end
 end
