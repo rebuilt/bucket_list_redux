f1b5c8397ff41a4ed0ac8afc85b5db8d99dcf6b3
rebuilt <memoryman51@hotmail.com>
2020-08-07 23:02:24 +0200

    add edit and update routes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+  before_action :ensure_admin, only: %i[edit update]
   def new
     @user = User.new
   end
@@ -13,9 +14,19 @@ class UsersController < ApplicationController
     end
   end
 
+  def edit
+    @user = User.find params[:id]
+  end
+
+  def update; end
+
   private
 
   def user_params
     params.require(:user).permit(:email, :password)
+  end
+
+  def ensure_admin
+    redirect_to account_path unless current_user.role == 'admin'
   end
 end
