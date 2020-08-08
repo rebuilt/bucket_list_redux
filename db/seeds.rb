480d22ab7f430b78af1ea773dbb6b6e433a2b9f3
rebuilt <memoryman51@hotmail.com>
2020-08-08 14:07:21 +0200

    added seed information

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,26 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+# create an admin User and a registered User.
+admin = User.create! email: 'neill@extensionschool.ch',
+                     password: 'password',
+                     name: 'NJ Pearman',
+                     role: 'admin'
+
+user1 = User.create! email: 'visitor@extensionschool.ch',
+                     password: 'password',
+                     name: 'Visitor'
+
+user2 = User.create! email: 'visitor2@extensionschool.ch',
+                     password: 'password',
+                     name: 'Visitor'
+
+user3 = User.create! email: 'visitor3@extensionschool.ch',
+                     password: 'password',
+                     name: 'Visitor'
+
+photo_url = 'http://fpoimg.com/255x170'
+5.times do |i|
+  Idea.create! user: admin,
+               title: "Neill's idea #{admin.name}. Idea #: #{i}", photo_url: photo_url
+  Idea.create! title: "Idea for user #{user1.name}. Idea #: #{i} ", user: user1, photo_url: photo_url
+  Idea.create! title: "Idea for user #{user2.name} Idea #: #{i} ", user: user2, photo_url: photo_url
+  Idea.create! title: "Idea for user #{user3.name} Idea #: #{i} ", user: user3, photo_url: photo_url
+end
