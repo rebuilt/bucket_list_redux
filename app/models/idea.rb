59f5b309fa09504025cda3e37b6cf2c3bb57b40d
rebuilt <memoryman51@hotmail.com>
2020-08-08 14:08:43 +0200

    added paginates_per usage

--- app/models/idea.rb
+++ app/models/idea.rb
@@ -6,10 +6,11 @@ class Idea < ApplicationRecord
   belongs_to :user
 
   has_and_belongs_to_many :users
+  paginates_per 9
 
   scope :search, ->(term) { title_contains(term).or(description_contains(term)) }
 
-  scope :most_recent, -> { all.order(created_at: :desc) }
+  scope :most_recent, -> { all.order(created_at: :desc).limit(3) }
 
   scope :title_contains, ->(term) { where('title LIKE ?', "%#{term}%") }
   scope :description_contains, ->(term) { where('description LIKE ?', "%#{term}%") }
