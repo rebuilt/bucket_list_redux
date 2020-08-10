1599da6e163b8b68a42dcc79ace51ee37ac258af
rebuilt <memoryman51@hotmail.com>
2020-08-10 13:56:42 +0200

    comment out unneeded goal variable

--- app/helpers/goals_helper.rb
+++ app/helpers/goals_helper.rb
@@ -2,6 +2,9 @@ module GoalsHelper
   def has_goal?(goal)
     return false unless logged_in?
 
+    # @goals ||= current_user.goals
+
+    # @goals.include? goal
     current_user.goals.include? goal
   end
 end
