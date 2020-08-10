2398445941d116487098513bcb9242a5c24d43cc
rebuilt <memoryman51@hotmail.com>
2020-08-10 21:14:28 +0200

    select only the id, and role for the user, preload the goals

--- app/helpers/goals_helper.rb
+++ app/helpers/goals_helper.rb
@@ -2,9 +2,17 @@ module GoalsHelper
   def has_goal?(goal)
     return false unless logged_in?
 
+    # this resulted in n+1 problem
+    # current_user.goals.include? goal
+    #
+    # this did not correct the n+1 problem, still created lots of requests
     # @goals ||= current_user.goals
-
     # @goals.include? goal
-    current_user.goals.include? goal
+    #
+    # this didn't work, resulted in undefinded method: includes
+    # @user ||= User.find(session[:user_id]).includes(:goals)
+
+    @user ||= User.select(:id, :role).includes(:goals).select(:id).find(session[:user_id])
+    @user.goals.include? goal
   end
 end
