6b142528a24fa8486864dd2be9a94ca87ac09945
rebuilt <memoryman51@hotmail.com>
2020-08-12 16:54:57 +0200

    added default_url_options and set_locale methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-
+  before_action :set_locale
   helper_method :logged_in?, :current_user
+
+  def default_url_options
+    { locale: I18n.locale }
+  end
 
   def logged_in?
     session[:user_id].present?
@@ -13,5 +17,9 @@ class ApplicationController < ActionController::Base
 
   def current_user
     @current_user ||= User.find(session[:user_id])
+  end
+
+  def set_locale
+    I18n.locale = params[:locale] || I18n.default_locale
   end
 end
