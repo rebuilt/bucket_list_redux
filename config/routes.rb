15e9a58aedb18e81961a68332a8b3cfe4886509e
rebuilt <memoryman51@hotmail.com>
2020-08-12 16:55:55 +0200

    set a locale

--- config/routes.rb
+++ config/routes.rb
@@ -1,36 +1,34 @@
 Rails.application.routes.draw do
-  # get 'photos/index'
-
-  # get 'photos/create'
-
-  get 'sessions/new'
-
   root to: 'home#index'
+  scope '/:locale' do
+    root to: 'home#index'
+    get 'sessions/new'
 
-  get 'home/index'
+    get 'home/index'
 
-  get 'styleguide', to: 'styles#atoms'
-  get 'styles/atoms'
-  get 'styles/molecules'
-  get 'styles/organisms'
+    get 'styleguide', to: 'styles#atoms'
+    get 'styles/atoms'
+    get 'styles/molecules'
+    get 'styles/organisms'
 
-  get 'account/ideas'
+    get 'account/ideas'
 
-  resources :users, only: %i[new create edit update] do
-    resources :goals, only: %i[create destroy]
+    resources :users, only: %i[new create edit update] do
+      resources :goals, only: %i[create destroy]
+    end
+
+    resources :ideas do
+      resources :comments
+      resources :photos, only: %i[index create]
+    end
+
+    resources :sessions, only: %i[new create destroy]
+
+    get 'login', to: 'sessions#new'
+    get 'signup', to: 'users#new'
+
+    get 'account', to: 'account#edit'
+    patch 'account', to: 'account#update'
+    get 'account/goals'
   end
-
-  resources :ideas do
-    resources :comments
-    resources :photos, only: %i[index create]
-  end
-
-  resources :sessions, only: %i[new create destroy]
-
-  get 'login', to: 'sessions#new'
-  get 'signup', to: 'users#new'
-
-  get 'account', to: 'account#edit'
-  patch 'account', to: 'account#update'
-  get 'account/goals'
 end
